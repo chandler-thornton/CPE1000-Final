d9641b04b2558077a60bd702875a82edf63ae9bc
chandler-thornton <chanman1218@outlook.com>
2022-05-08 16:05:44 -0400

    Initial Commit. No LED matrix functionality

---
+++ Raspberry Pi/CPE100_Final.py
@@ -0,0 +1,29 @@
+import time
+from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
+
+start_time = time.time()
+laptime_current = 0
+laptime_all= []
+laptime_avg = 0
+minutes = 0
+seconds = 0
+
+while (event.action != ACTION_HELD):
+    press = input() 
+    laptime_current = int((time.time()) - start_time)
+    start_time = time.time()
+    laptime_all.append(laptime_current)
+    minutes = laptime_current / 60
+    seconds = laptime_current % 60
+    print("minutes:", minutes)
+    print("seconds:", seconds)
+
+for x in laptime_all:
+    laptime_avg += x
+
+laptime_avg = laptime_avg / len(laptime_all)
+
+minutes = laptime_avg / 60
+seconds = laptime_avg % 60
+
+print("avg time:", minutes, " minutes,", seconds, " seconds")
